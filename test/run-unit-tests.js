292aaa766d59e48ef46c69fb1a32258f0aa151c8
Zyuxua <165749159+Zyuxua@users.noreply.github.com>
2025-10-12 15:25:35 +0800

    Delete test/run-unit-tests.js

--- test/run-unit-tests.js
+++
@@ -1,8 +0,0 @@
-console.log("Running unit tests...");
-if (process.env.FAIL_UNIT === "1") {
-  console.error("Unit tests failed (simulated).");
-  process.exit(1);
-}
-console.log("Unit tests passed.");
-process.exit(0);
-
